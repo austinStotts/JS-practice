e47b664a18de5cf58c752fdda0c44a61245a3dd6
Austin Stotts <austintstotts@gmail.com>
2018-12-22 11:15:33 -0600

    arrays man

--- challenge/arrays.js
+++ challenge/arrays.js
@@ -1,3 +1,11 @@
-const arrayception = (array) (
-  array.reduce()
-)
+// !!! not finished !!!
+
+[ [ [3] ] [ [ [4] ] ] ]
+
+const arrayception = (array) => {
+  if(array.includes(Array.isArray())) {
+    console.log('yes')
+  } else console.log('no')
+}
+
+console.log(arrayception([1[2[3]]]))
