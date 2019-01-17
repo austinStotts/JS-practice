df4ab71b2f5c420fbed8c1292c7b01953a53c2af
Austin Stotts <austintstotts@gmail.com>
2019-01-17 13:24:43 -0600

    linked-list

--- data structures/linkedList.js
+++ data structures/linkedList.js
@@ -1,23 +1,43 @@
-var LinkedList = function (value) {
-  this.tail = this.newNode(value, null);
-  this.head = this.newNode(value,this.tail);
+var LinkedList = function (value = null) {
+  this.head = this.newNode(value, null);
+  this.tail = this.head;
 };
 
 LinkedList.prototype.newNode = function (value, next) {
-  return ({value,next})
+  return ({value,next});
 }
 
 LinkedList.prototype.addToTail = function (value) {
-  this.tail.next = this.newNode(value, null);
-  this.tail = this.newNode(value, null);
+  if(this.head.value === null) {
+    this.head.value = value;
+  } else {
+    let node = this.head;
+    while(node.next !== null) {
+      node = node.next;
+    }
+    node.next = this.newNode(value, null);
+    this.tail = node.next;
+  }
 }
 
 LinkedList.prototype.removeHead = function () {
-
+  if(this.tail === this.head) {
+    delete this.head;
+    delete this.tail;
+  } else {
+    this.head = this.head.next;
+  }
 }
 
-LinkedList.prototype.contains = function () {
-
+LinkedList.prototype.contains = function (value) {
+  let node = this.head;
+  while (node) {
+    if(node.value === value) {
+      return true;
+    }
+    node = node.next;
+  }
+  return false;
 }
 
 
