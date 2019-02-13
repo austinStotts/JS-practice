de57c5982a69fed08bdbf9c8c01b857267ba0dfa
Austin Stotts <austintstotts@gmail.com>
2019-02-13 13:49:48 -0600

    stuff

---
+++ challenge/fraction-converter.js
@@ -0,0 +1,17 @@
+// in: 0.5
+// out: 1/2
+
+// in: 3
+// out: 3/1
+
+function fractionConverter (number) {
+  if(number % 1 === 0) return `${number}/1`
+  else {
+    return 'need to do more work...';
+  }
+}
+
+console.log(fractionConverter(3.3));
+
+// not finished 
+// !! !! !! !! !! !! !! !!
